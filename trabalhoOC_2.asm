dc1d3af5f4ed3d8a95230ef17b10aa41d8118bbb
Thiago Lopes <tiagojosecnn@outlook.com>
2023-06-17 18:26:44 -0300

    Adiciona nops.

--- trabalhoOC_2.asm
+++ trabalhoOC_2.asm
@@ -17,32 +17,38 @@ main:
     li      $a1, 20         # Segundo parâmetro: SIZE
     li      $a2, 71         # Terceiro parâmetro: 71
     jal     inicializaVetor # Chama a função inicializaVetor
+    nop
     move    $s1, $v0        # Guarda o retorno da função em soma
     
     # Chama a função imprimeVetor
     move    $a0, $s0        # Primeiro parâmetro: vet
     li      $a1, 20         # Segundo parâmetro: SIZE
     jal     imprimeVetor    # Chama a função imprimeVetor
+    nop
     
     # Chama a função ordenaVetor
     move    $a0, $s0        # Primeiro parâmetro: vet
     li      $a1, 20         # Segundo parâmetro: SIZE
     jal     ordenaVetor     # Chama a função ordenaVetor
+    nop
     
     # Chama a função imprimeVetor
     move    $a0, $s0        # Primeiro parâmetro: vet
     li      $a1, 20         # Segundo parâmetro: SIZE
     jal     imprimeVetor    # Chama a função imprimeVetor
+    nop
     
     # Chama a função zeraVetor
     move    $a0, $s0        # Primeiro parâmetro: &vet[0]
     addi    $a1, $s0, 80    # Segundo parâmetro: &vet[20]
     jal     zeraVetor       # Chama a função zeraVetor
+    nop
     
     # Chama a função imprimeVetor
     move    $a0, $s0        # Primeiro parâmetro: vet
     li      $a1, 20         # Segundo parâmetro: SIZE
     jal     imprimeVetor    # Chama a função imprimeVetor
+    nop
     
     # Impressão em tela: printf("Soma: %d\n", soma);
     li      $v0, 4          # Código 4 para impressão de string
@@ -71,15 +77,17 @@ zeraVetor:
     
     zeraLoop:
         bge    $a0, $a1, zeraFim    # Se inicio >= fim vai para zeraFim
+        nop
         sw      $zero, 0($a0)       # Salva valor 0 no endereço apontado por inicio
         addi    $a0, $a0, 4         # Incrementa inicio para a próxima posição
         j       zeraLoop            # Repete o laço
-    
+    	nop
+    	
     zeraFim:
 
     # Fim da função    
     jr      $ra             # Retorna
-    
+    nop
 
 imprimeVetor:
     # Esta função não é folha
@@ -100,6 +108,7 @@ imprimeVetor:
     
     imprimeLoop:
         beq     $s2, $s1, imprimeFim    # Se i == tam vai para imprimeFim
+        nop
         sll     $t0, $s2, 2         # $t0 = i * 4
         add     $t0, $s0, $t0       # $t0 = &vet[i]
         
@@ -113,6 +122,7 @@ imprimeVetor:
     
         addi    $s2, $s2, 1         # Incremento i++
         j       imprimeLoop         # Repete o laço
+        nop
                 
     imprimeFim:
     li      $v0, 11         # Código 11 para impressão de caractere
@@ -128,6 +138,7 @@ imprimeVetor:
     
     # Fim da função
     jr      $ra             # Retorna
+    nop
     
 
 inicializaVetor:
@@ -153,6 +164,7 @@ inicializaVetor:
     # Caso base da recursão
     move    $v0, $zero      # Prepara valor de retorno 0
     ble     $s1, $zero, inicializaFim   # Se tamanho <= 0 vai para inicializaFim
+    nop
     
     # Passo recursivo
     # Chama a função valorAleatorio
@@ -165,6 +177,7 @@ inicializaVetor:
     li      $t0, 3          # $t0 = 3
     sw      $t0, 0($sp)     # Quinto parâmetro: 3    
     jal     valorAleatorio
+    nop
     addi    $sp, $sp, 4     # Libera 4 bytes na pilha do quinto parâmetro
     
     move    $s3, $v0        # novoValor = $v0 (retorno da função valorAleatorio)
@@ -179,6 +192,7 @@ inicializaVetor:
     addi    $a1, $s1, -1    # Segundo parâmetro: tamanho - 1
     move    $a2, $s3        # Terceiro parâmetro: novoValor
     jal     inicializaVetor
+    nop
     
     # Prepara valor de retorno
     add     $v0, $v0, $s3   # Prepara valor de retorno novoValor + retorno da recursão 
@@ -194,6 +208,7 @@ inicializaVetor:
     
     # Fim da função
     jr      $ra             # Retorna
+    nop
     
 
 ordenaVetor:
@@ -222,12 +237,14 @@ ordenaVetor:
     ordenaFor1:
         addi    $t0, $s1, -1            # $t0 = n - 1
         bge     $s2, $t0, ordenaFim1    # Se i >= n - 1 vai para ordenaFim1
+        nop
         move    $s4, $s2                # min_idx = i
     
         # Laço interno
         addi    $s3, $s2, 1             # j = i + 1
         ordenaFor2:
             bge     $s3, $s1, ordenaFim2    # Se j >= n vai para ordenaFim2
+            nop
                     
             # Condicional dentro do lanço interno
             # Leitura do valor de vet[j]
@@ -241,15 +258,18 @@ ordenaVetor:
             lw      $t1, 0($t1)             # $t1 = vet[min_idx]
             
             bge     $t0, $t1, sortIf1Fim    # Se vet[j] >= vet[min_idx] vai para sortIf1Fim
+            nop
             move    $s4, $s3                # min_idx = j                        
             
             sortIf1Fim:
             addi    $s3, $s3, 1             # j++
             j       ordenaFor2              # Repete o laço interno
-        
+            nop
+            
         ordenaFim2:
         # Condicional após o laço interno
         beq     $s4, $s2, ordenaIfFim       # Se min_idx == i vai para ordenaIfFim
+        nop
         
         # Chama função troca
         sll     $t0, $s4, 2             # $t0 = min_idx * 4
@@ -257,11 +277,13 @@ ordenaVetor:
         sll     $t0, $s2, 2             # $t0 = i * 4
         add     $a1, $s0, $t0           # Segundo parâmetro: &vet[i]
         jal     troca
+        nop
         
         ordenaIfFim:
         addi    $s2, $s2, 1             # i++
         j       ordenaFor1              # Repete o laço externo
-    
+    	nop
+    	
     ordenaFim1:
     # Libera espaço na pilha
     lw      $ra, 0($sp)     # Recupera $ra da pilha
@@ -274,7 +296,8 @@ ordenaVetor:
     
     # Fim da função
     jr      $ra             # Retorna
-                  
+    nop
+              
 troca:
     # Esta função é folha
     # O primeiro parâmetro é o ponteiro para a posição a no vetor
@@ -282,6 +305,7 @@ troca:
     
     # Teste da condicional
     beq     $a0, $a1, trocaFim  # Se a == b vai para trocaFim
+    nop
     
     # Troca de valores
     lw      $t0, 0($a0)     # $t0 = *a
@@ -292,6 +316,7 @@ troca:
     trocaFim:
     # Fim da função
     jr      $ra             # Retorna
+    nop
                       
                                                                                                                                                                                                                                                           
 valorAleatorio:
@@ -311,5 +336,6 @@ valorAleatorio:
         
     # Fim da função
     jr      $ra             # Retorna
-
+    nop
+    
 
